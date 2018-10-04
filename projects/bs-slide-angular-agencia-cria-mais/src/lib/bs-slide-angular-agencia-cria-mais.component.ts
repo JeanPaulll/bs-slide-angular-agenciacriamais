be588ff810e5d528f1ea0675854144ac37a80295
Jean Paul <jeanpaulwebb@gmail.com>
2018-10-04 13:52:27 -0300

    Angular JSON

--- projects/bs-slide-angular-agencia-cria-mais/src/lib/bs-slide-angular-agencia-cria-mais.component.ts
+++ projects/bs-slide-angular-agencia-cria-mais/src/lib/bs-slide-angular-agencia-cria-mais.component.ts
@@ -1,83 +1,28 @@
 import {AfterViewInit, Component, ElementRef, HostBinding, Input, OnDestroy, OnInit} from '@angular/core';
 import {SlideInterface} from './models/slide-interface';
-// import exec from 'script-loader!../../../node_modules/jquery/dist/jquery.min.js';
 
-// import * as jQuery from 'jquery';
-// window['jQuery'] = jQuery;
-// declare var $: any;
-// import 'owl.carousel/dist/assets/owl.carousel.css';
-// import 'owl.carousel';
-
-
-// const webpack = require('webpack');
-// new webpack.ProvidePlugin({
-//     $: "jquery",
-//     jQuery: "jquery",
-//     "window.jQuery": "jquery"
-// })
-
-// import * as jQuery from 'jquery';
-// // import 'imports?jQuery=jquery!owl.carousel';
-
-// import 'imports-loader!jquery';
-// import 'imports-loader!owl.carousel';
-
-// declare var $: any;
-
-// import * as $ from 'jquery';
-
-// import 'imports-loader?jQuery=jquery!owl.carousel';
-// import exec from 'script-loader!../../../node_modules/jquery/dist/jquery.min.js';
-
-
+declare const $: any;
+declare const jQuery: any;
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel';
 // window['$'] = $;
 // window['jQuery'] = $;
-// import '../../node_modules/jquery/dist/jquery.js';
-
-// import * as $ from 'jquery';
-// // import 'imports-loader?jQuery=jquery!owl.carousel';
-// // import * as $ from 'jquery/dist/jquery.min.js';
-//
-//
-// declare const $: any;
-// import 'owl.carousel';
-// declare const owlCarousel: any;
-
-
-// You add it here
-
-
-import 'jquery';
-// import 'owl.carousel';
-declare const $: any; 
-
-// interface JQuery {
-//     owlCarousel(): JQuery;
-// }
-// import 'owl.carousel';
-// require('style!redactor/redactor.css');
-// import 'imports?jQuery=jquery!owl.carousel';
-// import 'imports?jQuery=jquery!owl.carousel';
-
-// import * as $ from 'jquery';
-// import $ from 'jquery';
-// import * as $ from "jquery";
-
-// import * as $ from 'jquery';
-
-// import * as $ from 'jquery';
-// import 'imports?jQuery=jquery!owl.carousel';
-// import 'owl.carousel/dist/assets/owl.carousel.css';
-// import 'owl.carousel';
-// import * as owlCarousel from 'owl.carousel';
-// import owlCarousel from 'owl.carousel'
-// declare module "jquery" {
-//     export {$ as default};
-// }
 
 @Component({
     selector: 'bs-slide-angular-agcm',
     template: `
+        <!-- Set up your HTML -->
+        <!--<div class="owl-carousel">-->
+            <!--<div> Your Content </div>-->
+            <!--<div> Your Content </div>-->
+            <!--<div> Your Content </div>-->
+            <!--<div> Your Content </div>-->
+            <!--<div> Your Content </div>-->
+            <!--<div> Your Content </div>-->
+            <!--<div> Your Content </div>-->
+        <!--</div>-->
+   
+        
         <div class="top-feature section-spacing" *ngIf="slideImages?.length">
             <div class="top-features-slide">
                 <div class="item" *ngFor="let item of slideImages; index as i">
@@ -90,20 +35,21 @@ declare const $: any;
             </div>
         </div>
 
-        <div *ngIf="!slideImages?.length">
-            <div class="alert alert-info custom-alerts fade in">
-                Não há slides cadastradas.
-            </div>
-        </div>
+        <!--<div *ngIf="!slideImages?.length">-->
+            <!--<div class="alert alert-info custom-alerts fade in">-->
+                <!--Não há slides cadastradas.-->
+            <!--</div>-->
+        <!--</div>-->
 
-        <hr>
-        <pre>
-            {{ slideImages | json}}
-        </pre>
+        <!--<hr>-->
+        <!--<pre>-->
+            <!--{{ slideImages | json}}-->
+        <!--</pre>-->
     `,
     styles: []
 })
 export class BsSlideAngularAgenciaCriaMaisComponent implements OnInit, AfterViewInit, OnDestroy {
+
     get slideImages(): SlideInterface[] {
         return this._slideImages;
     }
@@ -113,11 +59,11 @@ export class BsSlideAngularAgenciaCriaMaisComponent implements OnInit, AfterView
         this._slideImages = value;
     }
 
-
     constructor(private el: ElementRef) {
+        console.log('rrrrr', $);
     }
 
-    @HostBinding('class') defaultClass = 'owl-carousel';
+    // @HostBinding('class') defaultClass = 'owl-carousel';
     @Input() options: object;
 
     $owlElement: any;
@@ -137,64 +83,65 @@ export class BsSlideAngularAgenciaCriaMaisComponent implements OnInit, AfterView
     ];
 
     private _slideImages: SlideInterface[];
-    calendarElement: any;
 
     ngOnInit() {
-        // console.log('jQuery', jQuery);
         console.log('this.slideImages', this.slideImages);
-
         this.slideImages = this.__slideImages;
     }
-    // calendarElement: JQuery;
-
 
     ngAfterViewInit(): void {
         debugger;
-        // this.calendarElement = $(this.elementRef.nativeElement);
-        // this.calendarElement.owlCarousel({});
+        // this.$owlElement = $(this.el.nativeElement);
+        // this.$owlElement.owlCarousel(this.defaultOptions);
 
-        this.$owlElement = $(this.el.nativeElement).owlCarousel(this.defaultOptions);
+        // $(document).ready(function(){
+        //     $(".owl-carousel").owlCarousel();
+        // });
+
+        // this.$owlElement = $(this.el.nativeElement).owlCarousel(this.defaultOptions);
 
         console.log('owlCarousel',  this.$owlElement);
 
+        $(document).ready(function () {
+            (function ($) {
+                console.log('leu tudo');
+                const  tSlider = $('.top-features-slide');
+                // tSlider.hide();
+                debugger;
+                if (tSlider.length) {
+                    tSlider.owlCarousel({
+                        loop: true,
+                        nav: false,
+                        dots: false,
+                        autoplay: true,
+                        autoplayTimeout: 4000,
+                        smartSpeed: 1200,
+                        autoplayHoverPause: true,
+                        lazyLoad: true,
+                        responsive: {
+                            0: {
+                                items: 1
+                            },
+                            500: {
+                                items: 2
+                            },
+                            992: {
+                                items: 3
+                            },
+                            1300: {
+                                items: 4,
+                                autoplay: false,
+                            }
+                        },
+                    });
+                    debugger;
+                    console.log('tSlider', tSlider);
+                }
 
-        // jQuery(document).ready(function () {
-        //     (function ($) {
-        //         console.log('leu tudo');
-        //         const  tSlider = $('.top-features-slide');
-        //         // tSlider.hide();
-        //         if (tSlider.length) {
-        //             tSlider.owlCarousel({
-        //                 loop: true,
-        //                 nav: false,
-        //                 dots: false,
-        //                 autoplay: true,
-        //                 autoplayTimeout: 4000,
-        //                 smartSpeed: 1200,
-        //                 autoplayHoverPause: true,
-        //                 lazyLoad: true,
-        //                 responsive: {
-        //                     0: {
-        //                         items: 1
-        //                     },
-        //                     500: {
-        //                         items: 2
-        //                     },
-        //                     992: {
-        //                         items: 3
-        //                     },
-        //                     1300: {
-        //                         items: 4,
-        //                         autoplay: false,
-        //                     }
-        //                 },
-        //             });
-        //         }
-        //
-        //     })(jQuery);
-        // });
+            })(jQuery);
+        });
 
-            // const  tSlider = $('.top-features-slide');
+        // const  tSlider = $('.top-features-slide');
             // tSlider.hide();
             // if (tSlider.length) {
             //     tSlider.owlCarousel({
@@ -259,5 +206,4 @@ export class BsSlideAngularAgenciaCriaMaisComponent implements OnInit, AfterView
         this.$owlElement.owlCarousel('destroy');
         this.$owlElement = null;
     }
-
 }
